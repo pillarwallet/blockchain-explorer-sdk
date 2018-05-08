ef889d4022f47ba44a5c969aa8ae3cf06c31c31a
Rafael Cidade <rafael@2030.io>
2018-05-08 11:40:50 +0100

    bugfix

--- test/Index.test.js
+++ test/Index.test.js
@@ -41,12 +41,11 @@ describe('Index Unit Tests', () => {
     it('Expect getBalance to call requestProvider.getRequest', () => {
 
       requestProvider.getRequest = jest.fn()
-      sdk.getBalance("arg1","arg2","arg3")
+      sdk.getBalance("arg1","arg2")
 
       let payload  = { 
         address:         "arg1",
         asset:           "arg2",
-        contractAddress: "arg3"
       };
 
       expect(requestProvider.postRequest).toBeCalled();
@@ -60,13 +59,13 @@ describe('Index Unit Tests', () => {
         sdk.txHistory("arg1","arg2","arg3","arg4")
         let payload  = { 
           address1:   "arg1",
-          address1:   "arg2",
+          address2:   "arg2",
           asset:      "arg3",
           fromtmstmp: "arg4"
         };
 
         expect(requestProvider.postRequest).toBeCalled();
-        expect(requestProvider.getRequest).toBeCalledWith(process.env.BCX_GET_TXHISTORY, payload);
+        expect(requestProvider.getRequest).toBeCalledWith(process.env.BCX_TX_HISTORY, payload);
     
         });
   })
