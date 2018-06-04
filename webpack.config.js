4cadd07db822eb4df4fef5ea12dc78b290436d9d
Rostislav Chuklin <rostislavcn@gmail.com>
2018-06-04 11:09:06 +0100

    webpack build process setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  target: 'node',
+  mode: 'development',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "commonjs2"
+  }
+};
