f262275e997b61119f332ed4e469b7acc87ca5de
Rafael Cidade <rafael@2030.io>
2018-06-27 11:52:34 +0100

    url fix

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,7 +7,7 @@ exports.httpsURL = {
         "BCX_SERVER_URL": "https://bcx-dev.pillarproject.io",
         },
     "qa": {
-        "BCX_SERVER_URL": "https://bcx-dev.pillarproject.io",
+        "BCX_SERVER_URL": "http://bcx-qa.pillarproject.io:8000",
         },
     "test": {
         "BCX_SERVER_URL": "https://bcx-dev.pillarproject.io",
