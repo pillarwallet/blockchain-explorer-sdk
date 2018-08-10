898521a702e64aa84a63a8cd4d0ed0614ef27d56
Rafael Cidade <rafael@2030.io>
2018-08-10 11:34:22 +0100

    refactoring tests

--- test/providers.test.js
+++ test/providers.test.js
@@ -1,35 +1,38 @@
 const requestProvider = require('../src/providers/RequestProvider');
 const requestUtil = require('../src/util/RequestUtil');
 
-describe('Fetch unit Test', () => {
-  it('Expected to call fetchRequests with POST', () => {
-    requestUtil.fetchRequests = jest.fn();
-    requestProvider.postRequest('arg1', 'arg2');
+let fetchRequests = requestUtil.fetchRequests;
 
+describe('Fetch unit Test', () => {
+
+  beforeEach(() => {
+    requestUtil.fetchRequests = jest.fn();
+  });
+
+  afterEach(() => {
+    requestUtil.fetchRequests = fetchRequests;
+  });
+
+  it('Expected to call fetchRequests with POST', () => {
+    requestProvider.postRequest('arg1', 'arg2');
     expect(requestUtil.fetchRequests).toBeCalled();
     expect(requestUtil.fetchRequests).toBeCalledWith('arg1', 'arg2', 'POST', undefined);
   });
 
   it('Expected to call fetchRequests with GET', () => {
-    requestUtil.fetchRequests = jest.fn();
     requestProvider.getRequest('arg1', 'arg2');
-
     expect(requestUtil.fetchRequests).toBeCalled();
     expect(requestUtil.fetchRequests).toBeCalledWith('arg1', undefined, 'GET', 'arg2');
   });
 
   it('Expected to call fetchRequests with PUT', () => {
-    requestUtil.fetchRequests = jest.fn();
     requestProvider.putRequest('arg1', 'arg2');
-
     expect(requestUtil.fetchRequests).toBeCalled();
     expect(requestUtil.fetchRequests).toBeCalledWith('arg1', 'arg2', 'PUT', undefined);
   });
 
   it('Expected to call fetchRequests with DELETE', () => {
-    requestUtil.fetchRequests = jest.fn();
     requestProvider.deleteRequest('arg1', 'arg2');
-
     expect(requestUtil.fetchRequests).toBeCalled();
     expect(requestUtil.fetchRequests).toBeCalledWith('arg1', 'arg2', 'DELETE', undefined);
   });
