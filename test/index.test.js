1eaf3433047aae8004015e3c56f47692a870ae40
Rafael Cidade <rafael@pillarproject.io>
2019-07-02 11:03:37 +0100

    unit tests for txHistory (graph)

--- test/index.test.js
+++ test/index.test.js
@@ -28,102 +28,73 @@ const bcx = new SDK({
   apiUrl: API_URL,
 });
 
-let getRequest = requestProvider.getRequest;
+const postRequestRestore = requestProvider.postRequest;
+const getRequestRestore = requestProvider.getRequest;
 
 describe('BcxSdk Class', () => {
   beforeEach(() => {
     requestProvider.getRequest = jest.fn();
+    requestProvider.postRequest = jest.fn();
   });
 
   afterEach(() => {
-    requestProvider.getRequest = getRequest;
-  });
-
-  describe('.getBalance', () => {
-    it('makes a GET request to /wallet-client/balance endpoint with payload', () => {
-      const payload = {
-        address: 'arg1',
-        asset: 'arg2',
-      };
-
-      bcx.getBalance(payload);
-
-      expect(requestProvider.getRequest).toBeCalled();
-      expect(requestProvider.getRequest).toBeCalledWith(`${API_URL}/wallet-client/balance`, payload);
-    });
-
-    it('validates payload', async () => {
-      expect.assertions(3);
-
-      try {
-        await bcx.getBalance({});
-      } catch (e) {
-        expect(e).toBeInstanceOf(TypeError);
-        expect(e.message).toMatch(/data should have required property 'address'/);
-        expect(e.message).toMatch(/data should have required property 'asset'/);
-      }
-    });
+    requestProvider.getRequest = getRequestRestore;
+    requestProvider.postRequest = postRequestRestore;
   });
 
   describe('.txHistory', () => {
-    it('makes a GET request to /wallet/txhistory with payload', () => {
+    it('makes a POST request to /wallet/txhistory with payload', () => {
       const payload = {
         address1: 'arg1',
         address2: 'arg2',
-        asset:    'arg3',
+        asset: 'arg3',
         nbTx: 4,
-        fromIndex: 5
+        fromIndex: 5,
       };
 
       bcx.txHistory(payload);
-
-      expect(requestProvider.getRequest).toBeCalled();
-      expect(requestProvider.getRequest).toBeCalledWith(`${API_URL}/wallet-client/txhistory`, payload);
+      expect(requestProvider.postRequest).toBeCalled();
+      expect(requestProvider.postRequest).toBeCalledWith(
+        `${API_URL}/wallet-client/txhistory`,
+        {
+          from: 'arg1',
+          to: 'arg2',
+          asset: 'arg3',
+          limit: 4,
+          offset: 5,
+          protocol: 'Ethereum',
+          network: 'mainnet',
+        },
+      );
     });
 
-    it('validates payload', async () => {
-      expect.assertions(2);
-
-      try {
-        await bcx.txHistory({});
-      } catch (e) {
-        expect(e).toBeInstanceOf(TypeError);
-        expect(e.message).toMatch(/data should have required property 'address1'/);
-      }
-    });
-
-    it('defaults payload properties', () => {
-      const address1 = 'address1';
-
-      bcx.txHistory({ address1 });
-
-      const payload = requestProvider.getRequest.mock.calls[0][1];
-      expect(payload).toEqual({
-        address1,
-        address2: 'ALL',
-        asset: 'ALL',
-        nbTx: 10,
-        fromIndex: 0
-      });
+    it('makes a POST request to /wallet/txhistory with no payload', () => {
+      bcx.txHistory({});
+      expect(requestProvider.postRequest).toBeCalled();
+      expect(requestProvider.postRequest).toBeCalledWith(
+        `${API_URL}/wallet-client/txhistory`,
+        {
+          protocol: 'Ethereum',
+          network: 'mainnet',
+        },
+      );
     });
 
     describe('.gasInfo', () => {
-
       it('makes a GET request to /wallet-client/gasinfo with a payload', () => {
         const payload = {
           nBlocks: 10,
         };
-  
+
         bcx.gasInfo(payload);
-  
+
         expect(requestProvider.getRequest).toBeCalled();
         expect(requestProvider.getRequest).toBeCalledWith(`${API_URL}/wallet-client/gasinfo`, payload);
       });
 
       it('makes a GET request to /wallet-client/gasinfo with no payload', () => {
-  
         bcx.gasInfo();
-  
+
         expect(requestProvider.getRequest).toBeCalled();
         expect(requestProvider.getRequest).toBeCalledWith(`${API_URL}/wallet-client/gasinfo`, {});
       });
@@ -131,13 +102,11 @@ describe('BcxSdk Class', () => {
 
     describe('.gasStation', () => {
       it('makes a GET request to /wallet-client/gasstation', () => {
-  
         bcx.gasStation();
 
         expect(requestProvider.getRequest).toBeCalled();
         expect(requestProvider.getRequest).toBeCalledWith(`${API_URL}/wallet-client/gasstation`, {});
       });
-    })
-
+    });
   });
 });
